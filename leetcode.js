601bc26fc516d83f0d86e08914b767b67ad64c0c
vishnugprasad <vishnuanugraha001@gmail.com>
2022-06-09 20:39:52 +0530

    leetcode subset

--- leetcode.js
+++ leetcode.js
@@ -207,3 +207,23 @@ var twoSum = function(nums, target) {
 
 console.log(twoSum(nums,target));
 
+
+
+var subsetsWithDup = function(nums) {
+    var res = [];
+    nums.sort((a, b) => a - b);
+    helper(nums, 0, res, []);
+    return res;
+  };
+  
+  var helper = function (nums, start, res, now) {
+    res.push(Array.from(now));
+  
+    for (var i = start; i < nums.length; i++) {
+      if (i === start || nums[i] !== nums[i - 1]) {
+        now.push(nums[i]);
+        helper(nums, i + 1, res, now);
+        now.pop();
+      }
+    }
+  };
