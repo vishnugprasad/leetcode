0a1aa8e9f84083d58859e9742546241e14ba53fc
vishnugprasad <vishnuanugraha001@gmail.com>
2022-06-14 21:25:12 +0530

    leetcode

--- intr.js
+++ intr.js
@@ -12,24 +12,59 @@
      
 
 
-var person = {
-    firstname: "Anna",
-    lastname: "Delvi",
-    getFullName : function(){
-        var fullName = this.firstname + this.lastname
-        return fullName;
+// var person = {
+//     firstname: "Anna",
+//     lastname: "Delvi",
+//     getFullName : function(){
+//         var fullName = this.firstname + this.lastname
+//         return fullName;
+//     }
+// }
+
+
+// console.log(person.getFullName());
+
+
+// var logName = function(lang1,lang2){
+//     console.log("logged:" + this.getFullName());
+//     console.log("argument:" + lang1 + " " + lang2);
+// }
+
+
+// var logPersonName = logName.bind(person);
+// logPersonName("en", "es");
+
+
+
+// var value
+// var map = {}
+// map[value] = 1;
+// console.log(map[1]);
+
+
+var obj = {name: "vishnu",
+            admno: 23}
+
+            obj.id = 3
+
+
+
+            
+
+var nums = [2,7,11,15]
+
+var twoSum = function(nums, target) {
+    var map = {};
+    for(var i = 0; i <nums.length; i++) {
+        var value = nums[i];
+        var complementpair = target - value;
+        if(map[complementpair] !== undefined) {
+            return [map[complementpair],i];
+            
+        }
+        else {
+            map[value] = i;
+        }
     }
-}
-
-
-console.log(person.getFullName());
-
-
-var logName = function(lang1,lang2){
-    console.log("logged:" + this.getFullName());
-    console.log("argument:" + lang1 + " " + lang2);
-}
-
-
-var logPersonName = logName.bind(person);
-logPersonName("en", "es");
+    
+};
